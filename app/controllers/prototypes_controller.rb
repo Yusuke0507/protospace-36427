43f95090336c18d35a15f46d76a1a7f4eb11ae6b
Yusuke0507 <xitianxiongjie0@gmail.com>
2021-08-06 15:44:44 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,9 @@
 class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -24,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+      unless user_signed_in? && current_user.id == @prototype.user_id
+        redirect_to action: :index
+      end
   end
 
   def update
